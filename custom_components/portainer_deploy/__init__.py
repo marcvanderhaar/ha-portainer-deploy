8a81bb1346f2bb698f00882f65ac95a3e71d0904
marcvanderhaar <marcvanderhaar@gmail.com>
2026-07-18 00:02:54 +0200

    Create __init__.py

---
+++ custom_components/portainer_deploy/__init__.py
@@ -0,0 +1,11 @@
+"""Portainer Deploy custom integration."""
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.core import HomeAssistant
+
+
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    return True
+
+
+async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    return True
